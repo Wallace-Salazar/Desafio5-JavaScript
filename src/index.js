9fde94d885f2ed29abf3e35c323d1bd27a2e8ada
Wallace Oliveira <104787895+Wallace-Salazar@users.noreply.github.com>
2022-06-24 19:41:39 -0300

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,21 @@
+// Crie um array que receba 5 itens e exiba no console.
+let nome = ["Stéfany", "Leandro", "Dandara", "Joy", "Aline"];
+console.log(nome);
+
+// Utilize um método para adicionar um nome ao inicio do array.
+nome.unshift("Marina");
+
+// Utilize um método para remover o último nome do array.
+nome.pop();
+
+// Utilize um método para adicionar dois nomes ao fim do array.
+nome.push("Luan", "Mariana");
+
+// Utilize um método para remover o primeiro nome do array.
+nome.shift();
+
+// Utilize um método para organizar em ordem crescente o seguinte array:
+// let numbers = [7,5,6,3,8,9,2,1,4]
+const num = [7, 5, 6, 3, 8, 9, 2, 1, 4];
+num.sort();
+console.log(num);
